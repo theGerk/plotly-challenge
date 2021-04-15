aa8f7d1570ff60685e741f9de14892e612f017d9
arm <andy.mcrae21@gmail.com>
2021-04-15 12:47:07 -0500

    working on getting a plot to show up

--- static/js/app.js
+++ static/js/app.js
@@ -2,4 +2,46 @@ console.log('loaded app.js');
 
 d3.json('../../samples.json').then((importedData) => {
     console.log(importedData);
+
+    // starting by plotting just patient zero
+    console.log(importedData.names[0]);
+    var patientZero = importedData.names[0];
+
+
+    // pulling out labels
+    var otuIds = importedData.samples.filter(e => e.id === patientZero).map(e => e.otu_ids);
+    console.log('otu_ids');
+    console.log(otuIds);
+
+    // adding OTU to labels
+    for (i=0; i < otuIds[0].length; i++) {
+        otuIds[0][i] = `OTU ${otuIds[0][i]}`;
+    }
+
+    console.log(otuIds);
+    // pulling out values 
+    var sampleValues = importedData.samples.filter(e => e.id === patientZero).map(e => e.sample_values);
+    console.log('sample_values');
+    console.log(sampleValues);
+
+    // pulling out hover text
+    var otuLabels = importedData.samples.filter(e => e.id === patientZero).map(e => e.otu_labels);
+    console.log('otu_labels');
+    console.log(otuLabels);
+
+
+
+    var traceZero = {
+        y: sampleValues,
+        x: otuIds,
+        hoverText: otuLabels,
+        type: 'bar',
+        orientation: 'h'
+    };
+
+    var data = [traceZero];
+
+    Plotly.newPlot('bar', data);
+
+
 });
