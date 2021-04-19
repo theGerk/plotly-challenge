d831eeaf5e232dd8a5782d44052ffa4137f1431d
arm <andy.mcrae21@gmail.com>
2021-04-19 12:17:25 -0500

    making plotting into functions starting with bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,12 @@
 console.log('loaded app.js');
 
 
+// Function for plotting Bargraph
+function barGraph(patientId) {
+    d3.json('../../samples.json').then((importedData) => {
 
+    });
+}
 
 
 
@@ -30,77 +35,79 @@ function initialBar() {
         console.log(importedData.names[0]);
     
         // picking a patient to work with
-        var patientZeroNum = importedData.names[0];
+        var patientZero = importedData.names[0];
+
+        barGraph(patientZero);
     
-        // pulling out data for patient zero
-        var patientZero = importedData.samples.filter(s => s.id === patientZeroNum);
+        // // pulling out data for patient zero
+        // var patientZero = importedData.samples.filter(s => s.id === patientZeroNum);
     
     
-        // console.log(patientZeroSorted);
+        // // console.log(patientZeroSorted);
     
-        console.log(patientZero);
+        // console.log(patientZero);
         
-        // how many slices of pie i want
-        var pieces = 10;
+        // // how many slices of pie i want
+        // var pieces = 10;
 
-        // pulling out labels
-        var otuIds = patientZero.map(e => e.otu_ids);
-        var otuIdsSlice = otuIds[0].slice(0,pieces);
-        console.log('otu_ids');
-        console.log(otuIdsSlice);
+        // // pulling out labels
+        // var otuIds = patientZero.map(e => e.otu_ids);
+        // var otuIdsSlice = otuIds[0].slice(0,pieces);
+        // console.log('otu_ids');
+        // console.log(otuIdsSlice);
         
-        var otuIdsSliceStr = [];
-        // adding OTU to labels
-        for (i=0; i < otuIdsSlice.length; i++) {
-            otuIdsSliceStr[i] = `OTU ${otuIdsSlice[i]}`;
-        }
+        // var otuIdsSliceStr = [];
+        // // adding OTU to labels
+        // for (i=0; i < otuIdsSlice.length; i++) {
+        //     otuIdsSliceStr[i] = `OTU ${otuIdsSlice[i]}`;
+        // }
     
-        // pulling out values 
-        var sampleValues = patientZero.map(e => e.sample_values);
-        var sampleValuesSlice = sampleValues[0].slice(0,pieces);
-        console.log('sample_values');
-        console.log(sampleValuesSlice);
+        // // pulling out values 
+        // var sampleValues = patientZero.map(e => e.sample_values);
+        // var sampleValuesSlice = sampleValues[0].slice(0,pieces);
+        // console.log('sample_values');
+        // console.log(sampleValuesSlice);
 
-        // pulling out labels
-        var otuLabels = patientZero.map(e => e.otu_labels);
-        var otuLabelsSlice = otuLabels[0].slice(0,pieces);
-        console.log('otu_labels');
-        console.log(otuLabelsSlice);
+        // // pulling out labels
+        // var otuLabels = patientZero.map(e => e.otu_labels);
+        // var otuLabelsSlice = otuLabels[0].slice(0,pieces);
+        // console.log('otu_labels');
+        // console.log(otuLabelsSlice);
     
-        // making my trace
-        var traceZero = {
-            // reversing so high values are on top
-            x: sampleValuesSlice.reverse(),
-            y: otuIdsSliceStr.reverse(),
-            type: 'bar',
-            // making horizontal
-            orientation: 'h',
-            text: otuLabelsSlice
-        };
+        // // making my trace
+        // var traceZero = {
+        //     // reversing so high values are on top
+        //     x: sampleValuesSlice.reverse(),
+        //     y: otuIdsSliceStr.reverse(),
+        //     type: 'bar',
+        //     // making horizontal
+        //     orientation: 'h',
+        //     text: otuLabelsSlice
+        // };
     
-        var data = [traceZero];
+        // var data = [traceZero];
     
-        var layout = {
-            title: 'belly button'
-        };
+        // var layout = {
+        //     title: 'belly button'
+        // };
     
-        Plotly.newPlot('bar', data, layout);
+        // Plotly.newPlot('bar', data, layout);
 
 
-        var traceBubble = {
-            x: otuIds[0],
-            y: sampleValues[0],
-            text: otuLabels[0],
-            mode: 'markers',
-            marker: {
-                color: otuIds[0],
-                size: sampleValues[0]
-            }
-        };
+        // var traceBubble = {
+        //     x: otuIds[0],
+        //     y: sampleValues[0],
+        //     text: otuLabels[0],
+        //     mode: 'markers',
+        //     marker: {
+        //         color: otuIds[0],
+        //         size: sampleValues[0]
+        //     }
+        // };
 
-        var dataBubble = [traceBubble];
+        // var dataBubble = [traceBubble];
 
-        Plotly.newPlot('bubble', dataBubble);
+        // Plotly.newPlot('bubble', dataBubble);
 
 
         // making plot for bubbles
