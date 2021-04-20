86967128b478a50f8b5438e74a8201ab09c8a1c6
Benji Altman <benji@altmansoftwaredesign.com>
2021-04-20 18:20:47 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 console.log('loaded app.js');
 
 
+d3.json('../samples.json').then(x => console.log(x));
+
 // Function for plotting Bargraph
 function barGraph(patientId) {
     d3.json('../samples.json').then((importedData) => {
